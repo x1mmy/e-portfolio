4d401738c740fc0503ed5f489b1ea6d956377c63
x1mmy <86193387+x1mmy@users.noreply.github.com>
2021-12-23 14:14:08 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+
+let contrastToggle = false;
+const scaleFactor = 1/20;
+
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX *  scaleFactor;
+    const y = event.clientY *  scaleFactor
+    console.log(x,y)
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const booInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * booInt}px, ${y * booInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+//template_8fscbph
+//service_w0yzu8w
+// user_nRCewh1MKR8sv2qYIc41r
+
+function contact(event) {
+    event.preventDefault ();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_w0yzu8w',
+            'template_8fscbph',
+            event.target,
+            'user_nRCewh1MKR8sv2qYIc41r'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch (() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert (
+                "The email service is temporarily unavailable. Please contact me directly on zimraan2012@gmail.com"
+            );
+        })
+}
+
+let isModalOpen = false;
+function toggleModal () {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
